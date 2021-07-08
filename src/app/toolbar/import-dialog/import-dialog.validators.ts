33f19562f49a19c735be89536342a8d2d5c41bf8
Michal Stano <mchl.stano@gmail.com>
2021-07-08 19:56:28 +0200

    Lint fix

--- src/app/toolbar/import-dialog/import-dialog.validators.ts
+++ src/app/toolbar/import-dialog/import-dialog.validators.ts
@@ -1,9 +1,9 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { ValidatorFn } from '@ngneat/reactive-forms';
 
 export function requiredFileType(): ValidatorFn {
   const correctType = 'json';
-  return function(control: AbstractControl) {
+  return (control: AbstractControl): ValidationErrors => {
     const file = control.value;
     if (file) {
       const extension = file.name.split('.')[1].toLowerCase();
@@ -21,7 +21,7 @@ export function requiredFileType(): ValidatorFn {
 }
 
 export function correctPortData(): ValidatorFn {
-  return function(control: AbstractControl) {
+  return (control: AbstractControl): ValidationErrors => {
     const result = control.value;
     if (result) {
       const areNodes = Array.isArray(result.nodes);
