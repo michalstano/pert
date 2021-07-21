4665ac234c5523e059fbfb724bfe4fca9385690d
Michal Stano <mchl.stano@gmail.com>
2021-07-21 20:00:42 +0200

    Improve css

--- src/app/toolbar/info-window/info-window.component.ts
+++ src/app/toolbar/info-window/info-window.component.ts
@@ -4,29 +4,33 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-info-window',
   template: `
     <h1>Legenda</h1>
-    <div>
-      <div class="key">esc</div>
-      <div>- opuść tryb edycji, łączenia, selekcji</div>
+    <div class="keys">
+      <div>
+        <div class="key">esc</div>
+        <div>- opuść tryb edycji, łączenia, selekcji</div>
+      </div>
+      <div>
+        <div class="key">del</div>
+        <div>- usuń węzeł/łączenie</div>
+      </div>
     </div>
-    <div>
-      <div class="key">del</div>
-      <div>- usuń węzeł/łączenie</div>
-    </div>
-    <div>
-      <div class="node selected"></div>
-      <div>- wybrany węzeł</div>
-    </div>
-    <div>
-      <div class="node edited"></div>
-      <div>- tryb edycji</div>
-    </div>
-    <div>
-      <div class="node invalid"></div>
-      <div>- nieprawidlowe dane w węźle</div>
-    </div>
-    <div>
-      <div class="node critical"></div>
-      <div>- węzeł w ścieżce krytycznej</div>
+    <div class="nodes">
+      <div>
+        <div class="node selected"></div>
+        <div>- wybrany węzeł</div>
+      </div>
+      <div>
+        <div class="node edited"></div>
+        <div>- tryb edycji</div>
+      </div>
+      <div>
+        <div class="node invalid"></div>
+        <div>- nieprawidlowe dane w węźle</div>
+      </div>
+      <div>
+        <div class="node critical"></div>
+        <div>- węzeł w ścieżce krytycznej</div>
+      </div>
     </div>
   `,
   styleUrls: ['./info-window.component.scss'],
